c2a55864e3eb2fcd85bba37b742d3c1de36723ce
marco <marco.tex@live.com>
2018-05-11 18:34:43 +0200

    costruzione oggetto job, macchina, operation

---
+++ Main.py
@@ -0,0 +1,147 @@
+
+import sys
+
+'''
+classe per modellare l'operazione
+'''
+class Task:
+
+    def __init__(self, n, eTime, pTask, cTask):
+        self.name = "task_" + str(n)
+        self.machine = None
+        self.executionTime = eTime
+        self.startTime = 0
+        self.finishTime = 0
+        self.pTask = pTask
+        self.cTask = cTask
+
+    def setParent(self, task):
+        self.pTask = task
+
+
+    def setChildren(self, task):
+        self.cTask = task
+
+    def setMachine(self, machine):
+        self.machine = machine
+
+    def __str__(self):
+        stringa = ""
+        stringa = "nome: " + self.name + "\n"
+        stringa += "macchina: " + (self.machine.name if self.machine is not(None) else "(nessuna)") + "\n"
+        stringa += "inizio: " + str(self.startTime) + "\n"
+        stringa += "fine: " + str(self.finishTime) + "\n"
+        stringa += "tempo esecuzione: " + str(self.executionTime) + "\n"
+        return stringa
+
+'''
+classe per modellare la macchina
+'''
+class Machine:
+
+    def __init__(self, n, tasks=[]):
+        self.name = "Machine_" + str(n)
+        self.tasks = tasks
+
+        for t in tasks:
+            t.setMachine(self)
+
+
+    ## metodo per aggiungere un task alla macchina
+    ## viene verificato
+    def addTask (self, t):
+
+        self.tasks.append(t)
+        t.setMachine(self)
+
+        # aggiornamento dei tempi
+        # verifica quale è il maggiore tra
+        # l'operazione sul job e sulla macchina
+        if t.pTask.finishTime < self.tasks[-1].finishTime:
+            t.startTime = self.tasks[-1].finishTime
+        else:
+            t.startTime = t.pTask.finishTime
+
+        t.finishTime = t.startTime + t.executionTime
+
+
+    def __str__(self):
+        stringa = ""
+        for t in self.tasks:
+            stringa += str(t) + "\n"
+
+        return stringa
+
+
+class Job:
+
+    def __init__(self, n, op_times):
+        self.name = "Job_" + str(n)
+        self.tasks = []
+
+        for i in range(len(op_times)):
+
+            name = str(n) + "_" + str(i)
+
+            # verifica se aggiungere il parent
+            if i == 0:
+                pTask = None
+            else:
+                pTask = self.tasks[i - 1]
+
+            # crea il task e lo aggiunge alla lista delle operazioni
+            t = Task(name, op_times[i], pTask, None)
+
+            if i == 0:
+                t.startTime = 0
+                t.finishTime = t.startTime + t.executionTime
+
+            else:
+                t.setParent(self.tasks[-1])
+                t.startTime = self.tasks[-1].finishTime
+                t.finishTime = t.startTime + t.executionTime
+
+            if i > 0:
+                self.tasks[-1].setChildren(t)
+
+            self.tasks.append(t)
+
+    def __str__(self):
+        stringa = self.name + "\n"
+        for t in self.tasks:
+            stringa += str(t) + '\n'
+
+        return stringa
+
+
+if __name__ == "__main__":
+
+    n_macchine = 3
+    n_jobs = 3
+
+    jobs_times =   [[3,2,2],
+                    [2,1,4],
+                    [1,2]]
+
+    '''
+    # test macchina con task
+    t_1 = Task(1, 10, None, None)
+    t_2 = Task(2, 20, None, None)
+    t_3 = Task(3, 30, None, None)
+
+    tasks = []
+    tasks.append(t_1)
+    tasks.append(t_2)
+    tasks.append(t_3)
+
+    machine_1 = Machine(1, tasks)
+    print(machine_1)
+    '''
+
+    ## aggiunta dei tempi ai jobs
+    for i in range(len(jobs_times)):
+        job = Job(str(i),jobs_times[i])
+        print(job)
+
+
+
---
+++ tabu_search.py
@@ -0,0 +1,65 @@
+import heapq
+import random
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.h_score = None
+
+    def generate_children(self, ):
+        """
+        Implement: generating children
+        """
+        children = [Node(self.value + random.randint(-5, 5)) for _ in xrange(3)]
+        return children
+
+    def __repr__(self, ):
+        return str(self.value)
+
+    def __hash__(self, ):
+        return str(self.value)
+
+
+# end Node
+
+
+def heuristic(node, goal=100):
+    """
+    Implement: calculating heuristic
+    """
+    node.h_score = abs(node.value - goal)  # Evaluate the node
+    return (node.h_score, node)
+
+
+# end heuristic
+
+
+def beam_search(origin, tabu_size=5, max_iterations=1000, max_children=4):
+    node_list = [(-1, origin)]
+    current_node = Node(None)  # init for while loop
+    tabu_set = []
+
+    while current_node.h_score != 0:
+        _, current_node = heapq.heappop(node_list)  # select the best node.
+        while current_node.value in tabu_set:
+            _, current_node = heapq.heappop(node_list)  # select the best node.
+        tabu_set = [current_node.value] + tabu_set[:tabu_size - 1]
+        print
+        current_node.value, tabu_set
+
+        for node in current_node.generate_children():
+            if node.value not in tabu_set:
+                heapq.heappush(node_list, heuristic(node))
+
+        node_list = node_list[:max_children]
+    # endwhile
+
+    return current_node
+
+
+# end beam_search
+
+
+print
+beam_search(Node(1))
