d3e1ad76468375749dbf62b8df9c820ee263c2ca
Simone <armari.simone@gmail.com>
2018-05-12 18:57:16 +0200

    Test di assegnamento dei task alle macchine effettuato con ERRORE.
    Il risultato ottenuto è che tutte le macchine hanno tutti i task nella lista dei task (macchina.tasks), dato che controllo che le macchine siano oggetti diversi e il ciclo di assegnamento viene eseguito in base al numero di elementi di jobs_times non capisco come da 8 task si ottenga un totale di 3*8 task divisi nelle liste "tasks" delle macchine (3 nel nostro esempio)
    
    Controllare gli output per capire meglio

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
-
 import sys
 
 '''
 classe per modellare l'operazione
 '''
+
+
 class Task:
 
     def __init__(self, n, eTime, pTask=None, cTask=None):
@@ -18,7 +19,6 @@ class Task:
     def setParent(self, task):
         self.pTask = task
 
-
     def setChildren(self, task):
         self.cTask = task
 
@@ -26,17 +26,19 @@ class Task:
         self.machine = machine
 
     def __str__(self):
-        stringa = ""
-        stringa = "nome: " + self.name + "\n"
-        stringa += "macchina: " + (self.machine.name if self.machine is not(None) else "(nessuna)") + "\n"
-        stringa += "inizio: " + str(self.startTime) + "\n"
-        stringa += "fine: " + str(self.finishTime) + "\n"
-        stringa += "tempo esecuzione: " + str(self.executionTime) + "\n"
+        stringa = "nome: " + self.name + "\n" +\
+                  "macchina: " + (self.machine.name if self.machine is not (None) else "(nessuna)") + "\n" +\
+                  "inizio: " + str(self.startTime) + "\n" +\
+                  "fine: " + str(self.finishTime) + "\n" +\
+                  "tempo esecuzione: " + str(self.executionTime) + "\n"
         return stringa
+
 
 '''
 classe per modellare la macchina
 '''
+
+
 class Machine:
 
     def __init__(self, n, tasks=[]):
@@ -46,10 +48,9 @@ class Machine:
         for t in tasks:
             t.setMachine(self)
 
-
     ## metodo per aggiungere un task alla macchina
     ## viene verificato
-    def addTask (self, t):
+    def addTask(self, t):
 
         t.setMachine(self)
 
@@ -57,7 +58,7 @@ class Machine:
         # verifica quale è il maggiore tra
         # l'operazione sul job e sulla macchina
 
-        if t.pTask is not(None) and len(self.tasks) > 0:
+        if t.pTask is not None and len(self.tasks) > 0:
             if t.pTask.finishTime < self.tasks[-1].finishTime:
                 t.startTime = self.tasks[-1].finishTime
             else:
@@ -69,13 +70,17 @@ class Machine:
 
         self.tasks.append(t)
 
-
     def __str__(self):
-        stringa = ""
+        stringa = self.name + "\n"
         for t in self.tasks:
             stringa += str(t) + "\n"
 
         return stringa
+
+
+'''
+classe per modellare il Job
+'''
 
 
 class Job:
@@ -84,25 +89,27 @@ class Job:
         self.name = "Job_" + str(n)
         self.tasks = []
 
+        # Per ogni tempo inserito devo creare un task
         for i in range(len(op_times)):
-
             name = str(n) + "_" + str(i)
 
             # crea il task e lo aggiunge alla lista delle operazioni
             t = Task(name, op_times[i])
 
+            # Se sto inserendo il primo task nel job calcolo i tempi
             if i == 0:
                 t.startTime = 0
                 t.finishTime = t.startTime + t.executionTime
 
+            # Se ci sono già altri task nel Job aggiorno la parentela e calcolo i tempi
+            #TODO capire se è giusto calcolare in questo momento i tempi di esecuzione o se vanno scelti quando si assegna il task alla macchina
             else:
                 t.setParent(self.tasks[-1])
+                self.tasks[-1].setChildren(t)
                 t.startTime = self.tasks[-1].finishTime
                 t.finishTime = t.startTime + t.executionTime
 
-            if i > 0:
-                self.tasks[-1].setChildren(t)
-
+            # Aggiungo il task appena creato alla lista del task del Job
             self.tasks.append(t)
 
     def __str__(self):
@@ -115,32 +122,45 @@ class Job:
 
 if __name__ == "__main__":
 
+    # Parametri di default
     n_macchine = 3
     n_jobs = 3
+    jobs_times = [[3, 2, 2],
+                  [2, 1, 4],
+                  [4, 3]]
+    assegnamento_macchine = [[0, 1, 2],
+                             [0, 2, 1],
+                             [1, 2]]
 
-    jobs_times =   [[3,2,2],
-                    [2,1,4],
-                    [1,2]]
+    # Inizializzazione delle strutture dati
+    macchine = []
+    jobs_list = []
+    for i in range(0, n_macchine):
+        macchine.append(Machine(i))
+    for i in range(0, n_jobs):
+        jobs_list.append(Job(i, jobs_times[i]))
 
-    '''
-    # test macchina con task
-    t_1 = Task(1, 10, None, None)
-    t_2 = Task(2, 20, None, None)
-    t_3 = Task(3, 30, None, None)
+    ''''# Stampa di prova
+    for job in jobs_list:
+        print(job)'''
 
-    tasks = []
-    tasks.append(t_1)
-    tasks.append(t_2)
-    tasks.append(t_3)
+    # Inserimento dei task dei Job nelle varie macchine secondo l'assegnamento impostato
+    for i in range(0, n_jobs):
+        for j in range(0, len(assegnamento_macchine[i])):
+            macchine[assegnamento_macchine[i][j]].addTask(jobs_list[i].tasks[j])
+            print("Selezionata macchina = " + macchine[assegnamento_macchine[i][j]].name + " " +
+                  "selezionato task = " + jobs_list[i].tasks[j].name + " " +
+                  "task nella macchina al momento = " + str(len(macchine[assegnamento_macchine[i][j]].tasks)))
 
-    machine_1 = Machine(1, tasks)
-    print(machine_1)
-    '''
+    # Controllo se ogni oggetto nella lista macchine è diverso dagli altri (se non si referenzia lo stesso oggetto)
+    if macchine[0] is macchine[1] or macchine[0] is macchine[2] or macchine[1] is macchine[2]:
+        print("Errore! gli oggetti nella lista macchine sono gli sessi")
+    else:
+        print("Gli oggetti sono diversi, OK!")
 
-    ## aggiunta dei tempi ai jobs
-    for i in range(len(jobs_times)):
-        job = Job(str(i),jobs_times[i])
-        print(job)
-
-    # TODO assegnare alle macchine i task dei job e controllare il ricalcolo dei tempi
-
+    # Controllo se la lista dei task è diversa per ogni macchina (devono essere diverse)
+    if set(macchine[0].tasks) == set(macchine[1].tasks) == set(macchine[2].tasks):
+        print("Errore! tutte le macchine hanno gli stessi task")
+    else:
+        print("Tutto ok")
+    # TODO capire perchè se le macchine sono oggetti diversi hanno la stessa identica lista di tasks come dimostrato da print precedenti
